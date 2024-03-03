66928635c0e48b57f92e5a1d57d83f74ece2f370
mhmsohel7 <mdsohelkhan230@gmail.com>
2024-03-03 17:45:16 +0600

    Menu Page Updated - and Order Now button added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -8,8 +8,11 @@ import soupImg from "../../assets/menu/soup-bg.jpg";
 import useMenu from "../../Hooks/useMenu";
 import SectionTitle from "../../Components/SectionTItle/SectionTitle";
 import MenuCategory from "./MenuCategory/MenuCategory";
+
 const Menu = () => {
+  // data load from custom hook "useMenu". Data gulu "menu" te destructure kora hoise.
   const [menu] = useMenu();
+  //then sob data filter kore category wise neya hocche.
   const desserts = menu.filter((item) => item.category === "dessert");
   const soup = menu.filter((item) => item.category === "soup");
   const salad = menu.filter((item) => item.category === "salad");
@@ -23,26 +26,30 @@ const Menu = () => {
 
       {/* Main Cover Image*/}
       <Cover img={menuImg} title="our menu"></Cover>
+
       <SectionTitle
         subHeading="Don't Miss"
         heading="Today's Offer"
       ></SectionTitle>
+
+      {/* New arekta <Menucategory/> Component make kore,oitate props hisebe "Filter kora" data patabo. then okane map kore data show korabo */}
       {/* Offered Menu Items */}
       <MenuCategory items={offered}></MenuCategory>
 
+      {/* Rest Menu Items start */}
       {/* Desserts Menu items */}
       <MenuCategory
         items={desserts}
-        title="Dessert"
+        title="dessert"
         img={dessertImg}
       ></MenuCategory>
-
       {/* Pizza Menu items */}
-      <MenuCategory items={pizza} title="Pizza" img={pizzaImg}></MenuCategory>
+      <MenuCategory items={pizza} title="pizza" img={pizzaImg}></MenuCategory>
       {/* Salad Menu items */}
-      <MenuCategory items={salad} title="Salad" img={saladImg}></MenuCategory>
+      <MenuCategory items={salad} title="salad" img={saladImg}></MenuCategory>
       {/* Soup Menu items */}
-      <MenuCategory items={soup} title="Soup" img={soupImg}></MenuCategory>
+      <MenuCategory items={soup} title="soup" img={soupImg}></MenuCategory>
+      {/* Rest Menu Items end */}
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Cover from "../../Shared/Cover/Cover";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
 
@@ -10,6 +11,15 @@ const MenuCategory = ({ items, title, img }) => {
         {items.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
+      </div>
+
+      {/* Order Now Button */}
+      <div className="text-center mt-6 mb-6">
+        <Link to="/order">
+          <button className="btn btn-outline border-0 border-b-4">
+            ORDER NOW
+          </button>
+        </Link>
       </div>
     </div>
   );
