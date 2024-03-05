fc88d6fb5303c1e6ef5fc7e4c1d279be63fbb8bb
mhmsohel7 <mdsohelkhan230@gmail.com>
2024-03-05 17:45:02 +0600

    Firebase project setup for Bistro-Boss - with Context AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./Routes/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Providers/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className="max-w-7xl mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-7xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
