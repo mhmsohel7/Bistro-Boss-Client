42ecc4c51d269ba27e7b9c84aea58a94db198ff8
mhmsohel7 <mdsohelkhan230@gmail.com>
2024-03-18 18:44:18 +0600

    Verify token & Axios Interceptor

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,6 +4,31 @@ const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  //request interceptor to add authorization header for every secure call to the api
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("request stop by interceptor", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  //intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      const status = error.response.status;
+      console.log("status error in interceptor", status);
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
