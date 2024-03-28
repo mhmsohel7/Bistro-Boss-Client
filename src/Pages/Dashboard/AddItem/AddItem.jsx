0470f289ce8943cb13c6b50b60382e8365b7f589
mhmsohel7 <mdsohelkhan230@gmail.com>
2024-03-28 19:18:57 +0600

    Design Form using DaisyUi and React-Hook-Form

--- src/Pages/Dashboard/AddItem/AddItem.jsx
+++ src/Pages/Dashboard/AddItem/AddItem.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../Components/SectionTItle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItem = () => {
   const { register, handleSubmit } = useForm();
@@ -14,23 +15,84 @@ const AddItem = () => {
       ></SectionTitle>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label>First Name</label>
-          <input {...register("firstName")} />
-          <select
-            defaultValue="default"
-            {...register("Category")}
-            className="select select-bordered w-full max-w-xs"
-          >
-            <option disabled value="default">
-              Select a Category
-            </option>
-            <option value="salad">Salad</option>
-            <option value="pizza">Pizza</option>
-            <option value="soup">Soup</option>
-            <option value="dessert">Dessert</option>
-            <option value="drinks">Drinks</option>
-          </select>
-          <input type="submit" />
+          <label className="form-control w-full mb-4">
+            <div className="label">
+              <span className="label-text">Recipe Name*</span>
+            </div>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              placeholder="Recipe Name"
+              className="input input-bordered w-full"
+            />
+          </label>
+
+          <div className="flex gap-6">
+            {/* Category start */}
+            <label className="form-control w-full mb-4">
+              <div className="label">
+                <span className="label-text">Category*</span>
+              </div>
+              <select
+                defaultValue="default"
+                {...register("category", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option disabled value="default">
+                  Select a Category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </label>
+            {/* Category end */}
+            {/* Price start */}
+            <label className="form-control w-full mb-4">
+              <div className="label">
+                <span className="label-text">Price*</span>
+              </div>
+              <input
+                {...register("price", { required: true })}
+                type="number"
+                placeholder="Price"
+                className="input input-bordered w-full"
+              />
+            </label>
+            {/* Price end */}
+          </div>
+
+          {/* Recipe details start */}
+          <label className="form-control mb-4">
+            <div className="label">
+              <span className="label-text">Recipe Details*</span>
+            </div>
+            <textarea
+              {...register("recipe")}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe details"
+            ></textarea>
+          </label>
+          {/* Recipe details end */}
+
+          {/* File Input */}
+          <label className="form-control w-full mb-4">
+            <div className="label">
+              <span className="label-text">Pick a file*</span>
+            </div>
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input file-input-bordered w-full"
+            />
+          </label>
+          {/* File input end */}
+
+          <button className="btn">
+            Add Item <FaUtensils />
+          </button>
         </form>
       </div>
     </div>
